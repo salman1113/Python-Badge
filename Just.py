44d82755491afbd46930f24d1709dd80afef79e5
salman <mrsalmanxzs@gmail.com>
2026-01-12 10:43:08 +0530

    file changed

--- Just.py
+++ Just.py
@@ -33,10 +33,37 @@ n = 5
 
 
 
-n = 5
-fact = 1
+# n = 5
+# fact = 1
 
-for i in range(1, n+1):
-    fact = fact * i
+# for i in range(1, n+1):
+#     fact = fact * i
 
-print(fact)
+# print(fact)
+
+
+
+# Remove duplicates from a list.
+
+lst = [1, 2, 2, 3, 4, 5, 5, 6, 2]
+# unique = []
+
+# for x in lst:
+#     if x not in unique:
+#         unique.append(x)
+
+# print(unique)
+
+# unique = list(set(lst))
+# print(unique)
+
+
+# Find the second largest number in a list.
+
+lst = [100, 200, 50, 100, 10]
+
+lst = list(set(lst))
+lst.sort()
+
+print(lst[-2])
+
