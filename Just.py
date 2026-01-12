a582a24b05b73efb0dbc07577876ad6b29c14a19
salman <mrsalmanxzs@gmail.com>
2026-01-12 10:29:38 +0530

    file changed

--- Just.py
+++ Just.py
@@ -23,10 +23,20 @@ n = 5
 
 # print(reverse)
 
-s = "salman"
-rev = ""
+# s = "salman"
+# rev = ""
 
-for i in s:
-    rev = i + rev
+# for i in s:
+#     rev = i + rev
 
-print(rev)
+# print(rev)
+
+
+
+n = 5
+fact = 1
+
+for i in range(1, n+1):
+    fact = fact * i
+
+print(fact)
